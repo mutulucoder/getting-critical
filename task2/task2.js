224f0f4570790454bcee61025fde20a6caff8dfb
mutulucoder <thabonyewe04@gmail.com>
2018-05-16 15:52:55 +0200

    First commit

---
+++ task2/task2.js
@@ -0,0 +1,12 @@
+var greenBox1 = document.getElementById('box1');
+var greenButton = document.getElementById('Button1');
+var redBox2 = document.getElementById('box2');
+var redButton = document.getElementById('Button2');
+
+ greenButton.addEventListener('click', function(){
+  box1.style.backgroundColor = 'green'
+})
+
+ redButton.addEventListener('click', function(){
+ box2.style.backgroundColor = 'red'
+})
---
+++ task3/task3.js~
@@ -0,0 +1,10 @@
+var myBox1 = document.getElementById('box1');
+var myBox2 = document.getElementById('box2');
+var button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+myBox1.style.backgroundColor = 'red'
+})
+button.addEventListener('click', function(){
+myBox2.style.backgroundColor = 'green'
+})
